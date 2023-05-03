73799dbf74e493b80c8e37c51a6fba141768e69e
Joshua Dean <dea16006@byui.edu>
2023-05-02 20:56:48 -0600

    Added cors policy

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express"); // We need Express.js
 const app = express(); // Declare our app
 
+// We have to set a cors policy so that a different domain can access it
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));
+
 app.use('/personalData', require('./routes')); // Use the home route
 
 app.listen(process.env.port || 3000); // We are listening on port 3000
