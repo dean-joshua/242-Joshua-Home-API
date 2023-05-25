0e7ccaf11093a4da9abc6fcedbefe703c0f2e657
Joshua Dean <dea16006@byui.edu>
2023-05-25 01:54:33 -0600

    Added personal site to cors origin

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const app = express();
 const cors = require('cors');
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://joshua-242-personalsite.onrender.com/',
+    ],
   })
 );
 
